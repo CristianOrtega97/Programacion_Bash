099e79c8d0bcfe58eb605546c4eb840eb0da73d1
Emanuel Camarena <emanuel.ortega@hotmail.com>
2020-09-01 00:13:02 -0500

    Mi primer script

---
+++ 1_utilityPostgres.sh
@@ -0,0 +1,9 @@
+# !bin/bash
+
+#Mi primer script del curso de automatizacion en bash
+
+#Programa para realizar algunas operaciones utilitarios de Postgres
+
+# Creado por: Emanuel Camarena
+
+echo "Bienvenido a mi programa"
