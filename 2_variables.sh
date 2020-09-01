b1bc30ca33fda6c13913cabdbd8eb9fa197b1d42
Emanuel Camarena <emanuel.ortega@hotmail.com>
2020-09-01 01:43:14 -0500

    Uso de variables entre scripts (export)

---
+++ 2_variables.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+# Variables de Sistema Operativo:
+#Las variables con las que ya cuenta el sistema operativo
+
+# Variables de usuario:
+#Las variables que son creadas por el usuario
+
+#Declaracion de variables
+
+#Llamado de variables
+echo "La variable del script anterior es: $nombre"
+
+#Las variables estan limitadas a existir solo en su script a menos que exista export es decir que tiene variables de otro
+
+
