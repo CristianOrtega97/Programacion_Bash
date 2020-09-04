c0481dffde678bf9e4c94147d371e6f4066391cd
Emanuel Camarena <emanuel.ortega@hotmail.com>
2020-09-04 12:32:14 -0500

    Dia 02

---
+++ 4_argumentos.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+#Creado por: Emanuel Camarena
+#Programa para ejemplificar el paso de argumentos
+
+nombreCurso=$1
+horarioCurso=$2
+
+echo "El nombre del curso es: $nombreCruso dictado en el horario $horarioCurso"
+
+echo "El número de parámetros enviados es $#"
+
+echo "Los parametros enviados son: $*"
---
+++ 5_comandos.sh
@@ -0,0 +1,9 @@
+# ! /bin/bash
+#Programa para revisar como ejecutar comandos dentro de un programa y almacenar en una variable para su posterior utilizacion  los tipos de operadores
+#Autor: Emanuel Camarena - @emanuel.dev
+
+ubicacionActual=`pwd`
+infoKernel=$(uname -a)
+
+echo "La ubicacion actual es: $ubicacionActual"
+echo "La informacion de Kernel es: $infoKernel"
